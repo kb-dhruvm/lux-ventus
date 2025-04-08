0f6331b7d08aae0daddb2ed27ae8728eafa6842f
Dhruv Maniya <dhruv.maniya@kombee.com>
2025-04-08 14:42:00 +0530

    live preview and structure

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,30 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
+import { UsersIcon } from "@sanity/icons";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("content")
+    .items([
+      S.documentTypeListItem("pages").title("Pages"),
+      S.divider(),
+      S.documentTypeListItem("categories").title("Categories"),
+      S.documentTypeListItem("posts").title("Posts"),
+      S.divider(),
+      S.documentTypeListItem("header").title("Header"),
+      S.documentTypeListItem("leftPannel").title("Left Pannel"),
+      S.divider(),
+      S.documentTypeListItem("hosts").title("Hosts / Authors").icon(UsersIcon),
+      ...S.documentTypeListItems().filter(
+        (item) =>
+          item.getId() &&
+          ![
+            "pages",
+            "posts",
+            "categories",
+            "hosts",
+            "header",
+            "leftPannel",
+          ].includes(item.getId()!)
+      ),
+    ]);
