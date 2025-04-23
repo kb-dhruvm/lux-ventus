85d5a12f6e89af94c2f697e7087c806d3e58ca69
Dhruv Maniya <dhruv.maniya@kombee.com>
2025-04-23 11:01:56 +0530

    implemented partytown scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 import { Poppins } from "next/font/google";
-import GoogleTagManagerScript from "@/components/layout/scripts/GoogleTagManagerScript";
+import { Partytown } from "@qwik.dev/partytown/react";
+import Script from "next/script";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -22,7 +23,43 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <GoogleTagManagerScript />
+        <Partytown forward={["dataLayer.push"]} />
+        <Script
+          type="text/partytown"
+          strategy="afterInteractive"
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-57NB65XM');
+          `,
+          }}
+        />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-57NB65XM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <Script
+          // type="text/partytown"
+          strategy="afterInteractive"
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r8gnemn6wn");
+          `,
+          }}
+        /> */}
       </head>
       <body className={`${poppins.variable} antialiased`}>{children}</body>
     </html>
